567f8fc49365fe5df56b08b2a30a09c363694d4d
Akhil Biju Varghese <akhil.sbj@gmail.com>
2025-01-14 20:59:30 +0530

    Add

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navigation bar
 var navlink = document.getElementById('navlink');
 function showmenu(){
     navlink.style.right = '0';
@@ -5,3 +6,42 @@ function showmenu(){
 function hidemenu(){
     navlink.style.right='-200px';
 }
+
+// Swiper Slider for Menu
+const swiper = new Swiper('.swiper-slider', {
+    grabCursor: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlide: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2 
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
