067bd4655be644c7d942280c890194ce3a153821
Akhil Biju Varghese <akhil.sbj@gmail.com>
2025-01-15 15:22:50 +0530

    add

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function hidemenu(){
     navlink.style.right='-200px';
 }
 
+// Scroll smooth
+window.addEventListener('scroll',reveal); 
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0; i<reveals.length; i++)
+        {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
 // Swiper Slider for Menu
 const swiper = new Swiper('.swiper-slider', {
     grabCursor: true,
